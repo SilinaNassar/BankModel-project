d1c79555624a94d5b5760657ef81d394c0ee8aee
silina nassar <nassar.silina@gmail.com>
2023-06-14 19:00:51 +0200

    BankModel -1

---
+++ project_hoobank-main/src/components/index.js
@@ -0,0 +1,18 @@
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Billing from './Billing';
+import Button from './Button';
+import CardDeal from './CardDeal';
+import Clients from './Clients';
+import CTA from './CTA';
+import FeedbackCard from './FeedbackCard';
+import Footer from './Footer';
+import GetStarted from './GetStarted';
+import Stats from './Stats';
+import Testimonials from './Testimonials';
+import Business from './Business';
+
+
+
+export {Navbar,Hero,Billing,Button,CardDeal,
+Clients,CTA,FeedbackCard,Footer,GetStarted,Stats,Testimonials,Business};
